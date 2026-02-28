57ab71116ca3654bd715cf898187f69a83dff3cc
omark <omarktee@gmail.com>
2026-02-28 22:01:32 +0000

    Add project files.

---
+++ ReactionTime/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ReactionTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Reaction_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Reaction_Button
+            // 
+            this.Reaction_Button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Reaction_Button.BackColor = System.Drawing.Color.Blue;
+            this.Reaction_Button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Reaction_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Reaction_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reaction_Button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Reaction_Button.Location = new System.Drawing.Point(0, 0);
+            this.Reaction_Button.Name = "Reaction_Button";
+            this.Reaction_Button.Size = new System.Drawing.Size(800, 450);
+            this.Reaction_Button.TabIndex = 0;
+            this.Reaction_Button.Text = "Click to begin the test.";
+            this.Reaction_Button.UseVisualStyleBackColor = false;
+            this.Reaction_Button.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Reaction_Button_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Reaction_Button);
+            this.Name = "Form1";
+            this.Text = "Reaction Time";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Reaction_Button;
+    }
+}
+
---
+++ ReactionTime/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ReactionTime
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random(); // Generates random delay time
+        Stopwatch stopwatch = new Stopwatch(); // Measures reaction speed
+        Timer gameTimer = new Timer(); // Controls when the button turns green
+        bool waitingForClick = false; // True when player has started a round (button is red)
+        bool canClick = false; // True when button is green and valid to click
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameTimer.Tick += GameTimer_Tick;
+        }
+
+        private void Reaction_Button_MouseClick(object sender, MouseEventArgs e)
+        {
+            // If button is green and player clicked in time
+            if (canClick)
+            {
+                stopwatch.Stop(); // Stop measuring time         
+                Reaction_Button.Text = $"Your time: {stopwatch.ElapsedMilliseconds} ms\nClick to try again"; // Show reaction time in milliseconds
+                Reaction_Button.BackColor = Color.Blue; // Reset background colour
+                canClick = false; // Not allowed to click until next round
+                waitingForClick = false; // Round is over 
+            } 
+            else
+            {
+                // If player clicks while waiting (button is red)
+                if (waitingForClick)
+                {
+                    gameTimer.Stop(); // Stop timer 
+                    Reaction_Button.Text = "Too early!\nTry again."; // Early click message
+                    Reaction_Button.BackColor = Color.Blue; // Reset background colour
+                    waitingForClick = false; // Reset round 
+                }
+                else
+                {
+                    // Start a new round
+
+                    Reaction_Button.BackColor = Color.Red; // Show waiting state 
+                    int delay = random.Next(2000, 5000); // Random delay (2–5 seconds) so user can't predict when to click
+                    gameTimer.Interval = delay; // Set timer delay 
+                    gameTimer.Start(); // Start countdown 
+                    waitingForClick = true; // Now waiting for green 
+                    Reaction_Button.Text = "Wait for green..."; 
+                }
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            gameTimer.Stop(); // Stop timer once triggered
+            Reaction_Button.BackColor = Color.Green; // Signal player to click
+            stopwatch.Restart(); // Start measuring reaction time
+            canClick = true; // Now player is allowed to click
+        }
+    }
+}
